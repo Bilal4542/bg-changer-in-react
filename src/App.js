0b061e27fb40493bbca4ec85225102feef77f55b
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2023-10-01 12:26:45 +0500

    bg-changer in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [color, setColor] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="wv-100 vh-100" style={{backgroundColor: color}}> 
+    <div className="d-flex fixed justify-content-center align-items-center p-5">
+      <div className="d-flex justify-content-center align-items-center bg-dark rounded p-2 ">
+        <button onClick={()=>setColor("red")} type="button" className="btn btn-danger rounded mx-4">Red</button>
+        <button onClick={()=>setColor("yellow")} type="button" className="btn btn-warning  rounded mx-4">Yellow</button>
+        <button onClick={()=>setColor("blue")} type="button" className="btn btn-primary rounded mx-4">Blue</button>
+        <button onClick={()=>setColor("skyBlue")} type="button" className="btn btn-info rounded mx-4">Sky Blue</button>
+        <button onClick={()=>setColor("green")} type="button" className="btn btn-success rounded mx-4">Green</button>
+        <button onClick={()=>setColor("black")} type="button" className="btn btn-dark bg-white text-dark rounded mx-4">Black</button>
+        <button onClick={()=>setColor("gray")} type="button" className="btn btn-secondary rounded mx-4">Gray</button>
+        <button onClick={()=>setColor("white")} type="button" className="btn btn-light border rounded mx-4">White</button>
+      </div>
     </div>
+    </div>
+    </>
   );
 }
 
